be838363b7bc725615c1cfdeeae6c85c3de1b7ec
Zahra Hassan <zahrahassanakhter@gmail.com>
2026-08-21 23:18:29 +0500

    save

--- leetcodemetrics.js
+++ leetcodemetrics.js
@@ -7,16 +7,34 @@ document.addEventListener("DOMContentLoaded", function () {
   let Easylabel = document.getElementById("Easy-label");
   let Mediumlabel = document.getElementById("Medium-progress-label");
   let Hardlabel = document.getElementById("Hard-label");
-
+  let statscontainer = document.querySelector(".statsconainer");
   function validateusername(username) {
     if (username.trim() === "") {
       alert("please enter a name to this field;");
     }
-    const isMatch=
+    let regexpression = "^[A-Za-z0-9_]{3,20}$";
+    let ismatch = regexpression.test(username);
+    if (!ismatch) {
+      return false;
+    }
+  }
+  async function fetchusername() {
+    let url = "";
+
+    try {
+      let response = await fetch(url);
+      let data = json.decode();
+      searchbtn.textContent = "searching...";
+      searchbtn.disabled = true;
+    } catch (error) {
+    } finally {
+    }
   }
 
   searchbtn.addEventListener("click", function () {
     const userInput = username.value;
-    validateusername(userInput);
+    if (validateusername(userInput)) {
+      fetchusername();
+    }
   });
 });
