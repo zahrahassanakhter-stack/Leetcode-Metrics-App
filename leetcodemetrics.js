3fbd5d2c21dedf000a7b9cb50efd34b1ca03db42
Zahra Hassan <zahrahassanakhter@gmail.com>
2026-08-24 23:36:43 +0500

    save

--- leetcodemetrics.js
+++ leetcodemetrics.js
@@ -1,40 +1,105 @@
 document.addEventListener("DOMContentLoaded", function () {
   let username = document.getElementById("inputfield");
   let searchbtn = document.getElementById("searchbar");
-  let Easyprogress = document.getElementById("Easy-progress");
-  let Mediumprogress = document.getElementById("Medium-progress");
-  let hardprogress = document.getElementById("Hard-progress");
+  let Easyprogresscircle = document.getElementById("Easy-progress");
+  let Mediumprogresscircle = document.getElementById("Medium-progress");
+  let hardprogresscircle = document.getElementById("Hard-progress");
   let Easylabel = document.getElementById("Easy-label");
-  let Mediumlabel = document.getElementById("Medium-progress-label");
+  let Mediumlabel = document.getElementById("Medium-label");
   let Hardlabel = document.getElementById("Hard-label");
-  let statscontainer = document.querySelector(".statsconainer");
+  let statscontainer = document.querySelector(".stats-container");
   function validateusername(username) {
     if (username.trim() === "") {
-      alert("please enter a name to this field;");
+      alert("Username is empty");
     }
-    let regexpression = "^[A-Za-z0-9_]{3,20}$";
+    let regexpression = /^[A-Za-z0-9_]{3,20}$/;
     let ismatch = regexpression.test(username);
     if (!ismatch) {
       return false;
+      console.log("hello");
     }
+    return ismatch;
   }
-  async function fetchusername() {
-    let url = "";
 
+  function EnterUserDetail(Parseddata) {
+    const totalQuestions = Parseddata.data.llQuestionsCount[0].count;
+    const totalEasyQuestions = Parseddata.data.llQuestionsCount[1].count;
+    const totalMediumQuestions = Parseddata.data.llQuestionsCount[2].count;
+    const totalHardQuestions = Parseddata.data.llQuestionsCount[3].count;
+
+    //solved question
+    const solvedQuestion =
+      Parseddata.data.matchedUser.submitStats.acSubmissionNum[0].count;
+    const solvedEasyQuestion =
+      Parseddata.data.matchedUser.submitStats.acSubmissionNum[1].count;
+    const solvedMediumQuestion =
+      Parseddata.data.matchedUser.submitStats.acSubmissionNum[2].count;
+    const solvedHardQuestion =
+      Parseddata.data.matchedUser.submitStats.acSubmissionNum[3].count;
+
+    calculateUserDetail(
+      totalQuestions,
+      solvedQuestion,
+      Easylabel,
+      Easyprogresscircle,
+    );
+    calculateUserDetail(
+      totalQuestions,
+      solvedQuestion,
+      Easylabel,
+      Easyprogresscircle,
+    );
+
+    calculateUserDetail(
+      totalQuestions,
+      solvedQuestion,
+      Easylabel,
+      Easyprogresscircle,
+    );
+
+    calculateUserDetail(
+      totalQuestions,
+      solvedQuestion,
+      Easylabel,
+      Easyprogresscircle,
+    );
+  }
+  async function fetchusername(username) {
     try {
-      let response = await fetch(url);
-      let data = json.decode();
       searchbtn.textContent = "searching...";
       searchbtn.disabled = true;
+      const proxyserver = "https://cors-anywhere.herokuapp.com/";
+      const url = "https://leetcode.com/graphql/";
+      const header = new Headers();
+      header.append("content-type", "application/json");
+      header.append("X-Requested-With", "XMLHttpRequest");
+      const graphql = JSON.stringify({
+        query:
+          "\n    query userSessionProgress($username: String!) {\n        allQuestionsCount {\n            difficulty\n            count\n        }\n        matchedUser(username: $username) {\n            submitStats {\n                acSubmissionNum {\n                    difficulty\n                    count\n                    submissions\n                }\n                totalSubmissionNum {\n                    difficulty\n                    count\n                    submissions\n                }\n            }\n        }\n    }",
+        variables: { username: `${username}` },
+      });
+      const request = {
+        method: "POST",
+        headers: header,
+        body: graphql,
+        redrirect: "follow",
+      };
+      let response = await fetch(proxyserver + url, request);
+      let Parseddata = await response.json();
+      console.log(Parseddata);
+      EnterUserDetail(Parseddata);
     } catch (error) {
+      statscontainer.innerHTML = "<p>No data found</p>";
     } finally {
+      searchbtn.textContent = "search";
+      searchbtn.disabled = false;
     }
   }
 
   searchbtn.addEventListener("click", function () {
     const userInput = username.value;
     if (validateusername(userInput)) {
-      fetchusername();
+      fetchusername(userInput);
     }
   });
 });
